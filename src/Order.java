252896deac5c31d4591c74aa08ed9208901a80d3
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-12-17 15:29:30 +0100

    Deleted methods that should be in Table class, remove useless methods and comments

--- src/Order.java
+++ src/Order.java
@@ -1,24 +1,23 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Order {
-	/**
-	 * 
-	 */
-	private final String[] STARTER = { "entrée", "salade", "soupe", "quiche", "aucune" };
-	private final String[] DISHES = { "plats", "poulet", "boeuf", "poisson", "végétarien", "vegan", "aucun" };
-	private final String[] SIDE_DISH = { "accompagnements", "riz", "pates", "frites", "légumes", "aucun" };
-	private final String[] DRINKS = { "boissons", "eau plate", "eau gazeuze", "soda", "vin", "aucune" };
-	private final String[] DESSERTS = { "desserts", "tarte maison", "mousse au chocolat", "tiramisu", "aucun" };
-
+	
 	public ArrayList<String> order;
+	
 	/**
-	 * 
+	 * Empty constructor 
 	 */
 	public Order() {
 		this.order = new ArrayList<String>();
 	}
 	
+	/**
+	 * Constructor with parameter
+	 * @param order
+	 */
+	public Order(ArrayList<String> order) {
+		this.order = new ArrayList<String>(order);
+	}
 	
 	/**
 	 * @return the order
@@ -40,28 +39,6 @@ public class Order {
 	
 	public void clearOrder() {
 		this.order.clear();
-	}
-
-	/**
-	 * Get list of items in the menu, depending on the type of dish
-	 * @param scan
-	 * @param info
-	 * @return what the user chose
-	 */
-	public int getInfos(Scanner scan, String info) {
-		System.out.println("choix " + info + " : ");
-		if (info.equalsIgnoreCase(this.STARTER[0]))
-			displayTable(this.STARTER);
-		else if (info.equalsIgnoreCase(this.DISHES[0]))
-			displayTable(this.DISHES);
-		else if (info.equalsIgnoreCase(this.SIDE_DISH[0]))
-			displayTable(this.SIDE_DISH);
-		else if (info.equalsIgnoreCase(this.DRINKS[0]))
-			displayTable(this.DRINKS);
-		else if (info.equalsIgnoreCase(this.DESSERTS[0]))
-			displayTable(this.DESSERTS);
-		System.out.println("que souhaitez vous comme " + info + " ? [saisir le chiffre correspondant]");
-		return scan.nextInt();
 	}
 	
 	/**
