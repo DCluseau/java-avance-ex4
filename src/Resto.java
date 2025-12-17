3695fce1ceca733dfccc7510830ea7e6ffdee466
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-12-17 15:31:02 +0100

    deleted old code and replaced it with call to Table class

--- src/Resto.java
+++ src/Resto.java
@@ -1,8 +1,3 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 /**
  * BaseTP2Resto programme simule la prise d'une commande dans un resto en ligne
  * par exemple avec choix d'entrées, plats, accompagnements, boissons et
@@ -13,94 +8,8 @@ import java.util.Scanner;
  */
 
 public class Resto {
-	public static final String[] STARTER = { "entrée", "salade", "soupe", "quiche", "aucune" };
-	public static final String[] DISHES = { "plats", "poulet", "boeuf", "poisson", "végétarien", "vegan", "aucun" };
-	public static final String[] SIDE_DISH = { "accompagnements", "riz", "pates", "frites", "légumes", "aucun" };
-	public static final String[] DRINKS = { "boissons", "eau plate", "eau gazeuze", "soda", "vin", "aucune" };
-	public static final String[] DESSERTS = { "desserts", "tarte maison", "mousse au chocolat", "tiramisu", "aucun" };
-
 	public static void main(String[] args) {
-		System.out.println("bonjour, combien de menus souhaitez vous ?");
-		Scanner scan = new Scanner(System.in);
-		int nbMenu;
-		while (scan.hasNextInt() == false)
-			scan.next();
-		nbMenu = scan.nextInt();
-		ArrayList<String> order = new ArrayList<String>();
-		ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
-		
-		for (int i = 0; i < nbMenu; i++) {
-			System.out.println("Commande numéro " + (i + 1));
-			int result = getInfos(scan, STARTER[0]);
-			if (STARTER.length - 1 > result)
-				order.add(STARTER[result]);
-
-			result = getInfos(scan, DISHES[0]);
-			if (DISHES.length - 1 > result)
-				order.add(DISHES[result]);
-
-			result = getInfos(scan, SIDE_DISH[0]);
-			if (SIDE_DISH.length - 1 > result)
-				order.add(SIDE_DISH[result]);
-
-			result = getInfos(scan, DRINKS[0]);
-			if (DRINKS.length - 1 > result)
-				order.add(DRINKS[result]);
-
-			result = getInfos(scan, DESSERTS[0]);
-			if (DESSERTS.length - 1 > result)
-				order.add(DESSERTS[result]);
-
-			System.out.println("Résumé de la commande " + (i + 1));
-			System.out.println(order); // ici on pourrait stocker la commande en base par exemple
-			System.out.println(); // avant de passer à la suivante
-			table.add(new ArrayList<String>(order));
-			order.clear();
-		}
-		scan.close();
-		printTicket(table);
-	}
-
-	public static int getInfos(Scanner scan, String info) {
-		System.out.println("choix " + info + " : ");
-		if (info.equalsIgnoreCase(STARTER[0]))
-			displayTable(STARTER);
-		else if (info.equalsIgnoreCase(DISHES[0]))
-			displayTable(DISHES);
-		else if (info.equalsIgnoreCase(SIDE_DISH[0]))
-			displayTable(SIDE_DISH);
-		else if (info.equalsIgnoreCase(DRINKS[0]))
-			displayTable(DRINKS);
-		else if (info.equalsIgnoreCase(DESSERTS[0]))
-			displayTable(DESSERTS);
-		System.out.println("que souhaitez vous comme " + info + " ? [saisir le chiffre correspondant]");
-		return scan.nextInt();
-	}
-
-	public static void displayTable(String[] table) {
-		for (int i = 1; i < table.length; i++) {
-			System.out.print("[" + i + " - " + table[i].toUpperCase() + "]");
-		}
-		System.out.println();
-	}
-
-	public static void printTicket(ArrayList<ArrayList<String>> table) {
-		FileWriter myWriter;
-		try {
-			myWriter = new FileWriter("ticket.txt");
-			for (int i = 0; i < table.size(); i++) {
-				myWriter.write("******************** Résumé de la commande N° " + (i + 1) + " ********************\n");
-				//myWriter.write("\n" + table.get(i) + "\n");
-				  for(String menu : table.get(i)) {
-					  myWriter.write(menu + "\n");
-				  }
-				  myWriter.write("\n");
-				 
-			}
-			myWriter.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Table table = new Table();
+		table.getOrders();
 	}
 }
